711bd5d95f73a4a8ca42a35a21fbe6f188b0f856
Vinh Hoang <64019402+vinhhn3@users.noreply.github.com>
2022-06-30 08:57:15 +0700

    Add project files.

---
+++ TCD0401AsyncProgramming/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+
+namespace TCD0401AsyncProgramming
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+
+    {
+      Thread mainThread = Thread.CurrentThread;
+      mainThread.Name = "Main Thread";
+      lock (Console.Out)
+      {
+        Console.WriteLine(mainThread.Name);
+      }
+      Thread thread1 = new Thread(() =>
+      {
+        CountDown("Timer 1...");
+      });
+      Thread thread2 = new Thread(() =>
+      {
+        CountUp("Timer 2...");
+      });
+
+      thread1.Start();
+      thread2.Start();
+
+    }
+
+    public static void CountDown(string message)
+    {
+      for (int i = 10; i >= 0; i--)
+      {
+        lock (Console.Out)
+        {
+          Console.ForegroundColor = ConsoleColor.Red;
+          Console.WriteLine($"{message}: {i} seconds");
+        }
+        Thread.Sleep(1000);
+        lock (Console.Out)
+        {
+          Console.ResetColor();
+
+        }
+      }
+    }
+
+    public static void CountUp(string message)
+    {
+      for (int i = 0; i <= 10; i++)
+      {
+        lock (Console.Out)
+        {
+          Console.ForegroundColor = ConsoleColor.Green;
+          Console.WriteLine($"{message}: {i} seconds");
+        }
+        Thread.Sleep(1000);
+        lock (Console.Out)
+        {
+          Console.ResetColor();
+
+        }
+      }
+    }
+  }
+}
